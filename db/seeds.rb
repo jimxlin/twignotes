8f0bca35ace4659d9efe586390c9eb867f68d85d
Jim X. Lin <miljinx@gmail.com>
2017-03-15 23:34:28 -0700

    Add sidebar, add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Note.delete_all
+Tagging.delete_all
+Tag.delete_all
+
+user = User.create(
+  email: 'foobar@foobar.com',
+  password: 'foobar',
+  password_confirmation: 'foobar'
+)
+
+hashtags = []
+8.times { hashtags << '#' + Faker::Hipster.word }
+mentions = []
+8.times { mentions << '@' + Faker::Name.first_name}
+
+32.times do
+  title = Faker::Hipster.sentence(rand(1..6))
+  (rand(0..1)).times { title += " #{hashtags.sample}" }
+  (rand(0..1)).times { title += " #{mentions.sample}" }
+  body = Faker::Hipster.paragraph(rand(2..4))
+  (rand(0..4)).times { body += " #{hashtags.sample}" }
+  (rand(0..4)).times { body += " #{mentions.sample}"}
+
+  user.notes.create(title: title, body: body)
+end
