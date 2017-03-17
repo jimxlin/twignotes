0b2ad035338f706e5378cfff3dd40e1db190ec3d
Jim X. Lin <miljinx@gmail.com>
2017-03-17 10:09:43 -0700

    Style note divs with proper widths

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,8 @@
 class TagsController < ApplicationController
   def index
     if current_user
-      render json: current_user.tags.order(:name).uniq
+      # get count, append that value to each element in array below
+      render json: current_user.tags.order(:name).uniq,
     else
       render json: nil, status: :ok
     end
