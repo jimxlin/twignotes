172264692e51e91e1abae1935b0adb99ec165e68
Jim X. Lin <miljinx@gmail.com>
2017-03-31 17:56:48 -0700

    Add js for new note submission

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
   def index
     if current_user
-      render json: current_user.tags
+      render json: current_user.tags.order(:name);
     else
       render json: nil, status: :ok
     end
