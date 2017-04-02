ec0817a9cd85a7ca9398bafcb489e96d39c0c0a0
Jim X. Lin <miljinx@gmail.com>
2017-04-01 17:15:53 -0700

    Tweak style, refactor notes controller for readability

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,7 +2,7 @@ class NotesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    render json: filtered_notes(params[:tags], params[:archive], params[:jointype])
+    render json: filtered_notes(params)
   end
 
   def create
@@ -33,29 +33,53 @@ class NotesController < ApplicationController
 
   private
 
-  UNTAGGED = 'UntaggedUntaggedUntaggedUntaggedUntaggedUntaggedUntaggedUntagged' # remove the need for this
+  def filtered_notes(params)
+    tags =     params[:tags]
+    archive =  params[:archive]
+    jointype = params[:jointype]
+    untagged = params[:untagged]
 
-  def filtered_notes(tags, archive, jointype)
-    if tags.nil?
-      archive == 'true' ? current_user.notes.archived : current_user.notes.unarchived
+    return get_all_notes(archive, untagged) if tags.nil?
+
+    tag_ids = tags.map {|t| t.to_i}
+    case jointype
+    when 'OR'
+      notes = get_or_tags_notes(tag_ids)
+    when 'AND'
+      notes = get_and_tags_notes(tag_ids)
     else
-      tag_ids = tags.map {|t| t.to_i}
-      if jointype == 'OR'
-        notes = current_user.notes.unarchived.
-          joins(:taggings).where(taggings: { tag_id: tag_ids })
-      elsif jointype == 'AND'
-        notes =
-          current_user.notes.includes(:tags).select do |note|
-            ( tag_ids - note.tags.map {|t| t.id} ).empty?
-          end
-      end
-      if tags.include?(UNTAGGED) # client requests untagged notes
-        untagged_notes = current_user.notes.unarchived.left_outer_joins(:taggings)
-          .where(taggings: { id: nil })
-        notes += untagged_notes
-      end
-      notes.to_a.uniq
+      raise Exception.new("Invalid join type was provided.")
     end
+
+    notes += get_untagged_notes if untagged == 'true'
+    notes.to_a.uniq
+  end
+
+  def get_all_notes(archive, untagged)
+    if archive == 'true'
+      current_user.notes.archived
+    elsif untagged == 'true'
+      current_user.notes.unarchived.
+        left_outer_joins(:taggings).where(taggings: { id: nil })
+    else
+      current_user.notes.unarchived
+    end
+  end
+
+  def get_or_tags_notes(tag_ids)
+    current_user.notes.unarchived.
+      joins(:taggings).where(taggings: { tag_id: tag_ids })
+  end
+
+  def get_and_tags_notes(tag_ids)
+    current_user.notes.includes(:tags).select do |note|
+      ( tag_ids - note.tags.map {|t| t.id} ).empty?
+    end
+  end
+
+  def get_untagged_notes
+    current_user.notes.unarchived.
+      left_outer_joins(:taggings).where(taggings: { id: nil })
   end
 
   def note_params
